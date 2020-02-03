6af64cd52c5fca3429281bf2fc0aaea4b1e5599d
er21jq <rene.van.bpi.putten@ing.com>
2020-02-03 17:18:25 +0100

    Added some comment on the approach

--- src/main/java/com/putoet/day9/Day9.java
+++ src/main/java/com/putoet/day9/Day9.java
@@ -7,6 +7,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.OptionalInt;
 
+/**
+ * Using a brute force approach ... load all distances in a map object, then get all cities on the map (in a Set),
+ * create a permutation of all cities (which provides basically a list  of all possible routes), and then find the
+ * min and max distance for each route.
+ */
 public class Day9 {
     public static void main(String[] args) {
         final List<String> descriptions = ResourceLines.list("/day9.txt");
