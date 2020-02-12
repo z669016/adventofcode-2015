8cc6789431338137cf4ee55fcde6d29d79486ea5
er21jq <rene.van.bpi.putten@ing.com>
2020-02-12 23:29:14 +0100

    Solved 21b

--- src/main/java/com/putoet/day21/Day21.java
+++ src/main/java/com/putoet/day21/Day21.java
@@ -1,6 +1,5 @@
 package com.putoet.day21;
 
-import utilities.Permutator;
 import utilities.ResourceLines;
 
 import java.util.*;
@@ -10,13 +9,16 @@ public class Day21 {
         final List<String> descriptions = ResourceLines.list("/day21.txt");
         final Armory armory = new Armory();
 
-        // Create a map of armament combinations to compare, use the costs as the key for the map
-        final List<Armory.Combination> combinations = armory.combinations();
-
-        // Get an ordered list of the keys, which means the first key that defeats the boss is the cheapest armament to win
+        List<Armory.Combination> combinations = armory.combinations();
         combinations.sort(Comparator.comparing(Armory.Combination::costs));
-        combinations.forEach(System.out::println);
+        tryWinningCombinations(descriptions, combinations);
 
+        combinations = armory.combinations();
+        combinations.sort(Comparator.comparing(Armory.Combination::costs).reversed());
+        tryLoosingCombinations(descriptions, combinations);
+    }
+
+    protected static void tryWinningCombinations(List<String> descriptions, List<Armory.Combination> combinations) {
         // Now, battle with the combinations until you have the player that wins (i.e. not the boss)
         for (Armory.Combination combi : combinations) {
             final Player you = new Player("You");
@@ -33,8 +35,25 @@ public class Day21 {
         }
     }
 
+    protected static void tryLoosingCombinations(List<String> descriptions, List<Armory.Combination> combinations) {
+        // Now, battle with the combinations until you have the player that wins (i.e. not the boss)
+        for (Armory.Combination combi : combinations) {
+            final Player you = new Player("You");
+            you.pick(combi.armaments());
+
+            final Player boss = Player.boss(descriptions);
+
+            final Player winner = battle(you, boss);
+            if (winner.name().equals(boss.name())) {
+                System.out.println("Boss won: " + you);
+                System.out.println("Costs have been " + combi.costs());
+                break;
+            }
+        }
+    }
+
     private static Player battle(Player you, Player boss) {
-        System.out.println("Battle between " + you + " and " + boss);
+        // System.out.println("Battle between " + you + " and " + boss);
         boolean defeated = false;
         Player winner = null;
         while (!defeated) {
@@ -52,7 +71,7 @@ public class Day21 {
             }
         }
 
-        System.out.println("Won by " + winner);
+        // System.out.println("Won by " + winner);
         return winner;
     }
 }
