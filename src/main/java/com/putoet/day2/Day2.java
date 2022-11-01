9112ceb36f36ad29aa64451103632f73d167f243
Rene van Putten <z669016@gmail.com>
2022-11-01 11:22:10 +0100

    Introduced AoC utilities

--- src/main/java/com/putoet/day2/Day2.java
+++ src/main/java/com/putoet/day2/Day2.java
@@ -1,18 +1,14 @@
 package com.putoet.day2;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import com.putoet.resources.ResourceLines;
+
+import java.util.List;
 
 public class Day2 {
-    public static void main(String[] args) throws URISyntaxException, IOException {
-        final URL url = Day2.class.getResource("/day2.txt");
-        final Path path = Paths.get(url.toURI());
+    public static void main(String[] args)  {
+        final List<String> lines = ResourceLines.list("/day2.txt");
 
-        System.out.println("Total wrapping paper required is " + Files.lines(path).map(Box::of).mapToInt(Box::totalWrap).sum());
-        System.out.println("Total feet of ribbon required is " + Files.lines(path).map(Box::of).mapToInt(Box::totalRibbon).sum());
+        System.out.println("Total wrapping paper required is " + lines.stream().map(Box::of).mapToInt(Box::totalWrap).sum());
+        System.out.println("Total feet of ribbon required is " + lines.stream().map(Box::of).mapToInt(Box::totalRibbon).sum());
     }
 }
