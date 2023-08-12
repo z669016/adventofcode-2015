fc99d2dffa8ca10c900fa9721c9845531175a04d
Rene van Putten <z669016@gmail.com>
2023-08-12 22:12:50 +0200

    Introduced 'var' and removed unnecessary print statements

--- src/main/java/com/putoet/day4/Day4.java
+++ src/main/java/com/putoet/day4/Day4.java
@@ -6,21 +6,17 @@ import java.security.NoSuchAlgorithmException;
 
 public class Day4 {
     public static void main(String[] args) throws NoSuchAlgorithmException {
-        final String input = "bgvyzdsv";
+        final var input = "bgvyzdsv";
         long code = 0;
         while (!MD5.hash(input + code).startsWith("00000")) {
-            System.out.print("\rCode: " + code);
             code++;
         }
-        System.out.println();
         System.out.println("Code " + code + " provides hash " + MD5.hash(input + code));
 
         code = 0;
         while (!MD5.hash(input + code).startsWith("000000")) {
-            System.out.print("\rCode: " + code);
             code++;
         }
-        System.out.println();
         System.out.println("Code " + code + " provides hash " + MD5.hash(input + code));
     }
 }
