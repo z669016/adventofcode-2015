5eadfe6218781f949d09d46a291db2050c1c6f55
Rene van Putten <z669016@gmail.com>
2023-08-13 08:15:51 +0200

    Added timing

--- src/main/java/com/putoet/day8/Day8.java
+++ src/main/java/com/putoet/day8/Day8.java
@@ -1,5 +1,6 @@
 package com.putoet.day8;
 
+import com.putoet.Timer;
 import com.putoet.resources.ResourceLines;
 import utilities.StringUtils;
 
@@ -9,17 +10,23 @@ public class Day8 {
     public static void main(String[] args) {
         final List<String> input = ResourceLines.list("/day8.txt");
         final int stringSizeSum = input.stream().mapToInt(String::length).sum();
-        final int sanitizedSizeSum = input.stream().mapToInt(s -> StringUtils.sanitize(s).length()).sum();
 
-        System.out.println("Solution day 8a");
-        System.out.println("Total length of the strings: " + stringSizeSum);
-        System.out.println("Total length of the sanitized strings: " + sanitizedSizeSum);
-        System.out.println("The difference: " + (stringSizeSum - sanitizedSizeSum));
+        Timer.run(() -> {
+            final int sanitizedSizeSum = input.stream().mapToInt(s -> StringUtils.sanitize(s).length()).sum();
+            System.out.println("Solution day 8a");
+            System.out.println("Total length of the strings: " + stringSizeSum);
+            System.out.println("Total length of the sanitized strings: " + sanitizedSizeSum);
+            System.out.println("The difference: " + (stringSizeSum - sanitizedSizeSum));
+            return null;
+        });
 
-        final int encodedSizeSum = input.stream().mapToInt(s -> StringUtils.encode(s).length()).sum();
-        System.out.println("Solution day 8b");
-        System.out.println("Total length of the strings: " + stringSizeSum);
-        System.out.println("Total length of the encoded strings: " + encodedSizeSum);
-        System.out.println("The difference: " + (encodedSizeSum - stringSizeSum));
+        Timer.run(() -> {
+            final int encodedSizeSum = input.stream().mapToInt(s -> StringUtils.encode(s).length()).sum();
+            System.out.println("Solution day 8b");
+            System.out.println("Total length of the strings: " + stringSizeSum);
+            System.out.println("Total length of the encoded strings: " + encodedSizeSum);
+            System.out.println("The difference: " + (encodedSizeSum - stringSizeSum));
+            return null;
+        });
     }
 }
