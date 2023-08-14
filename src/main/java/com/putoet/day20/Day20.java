37a84cec31f59b9606214d8fec7f7cec03477399
Rene van Putten <z669016@gmail.com>
2023-08-14 18:33:51 +0200

    Introduced Timer and var

--- src/main/java/com/putoet/day20/Day20.java
+++ src/main/java/com/putoet/day20/Day20.java
@@ -2,20 +2,21 @@ package com.putoet.day20;
 
 import java.util.function.Function;
 
+import com.putoet.Timer;
 import com.putoet.math.Factors;
 
 public class Day20 {
     public static void main(String[] args) {
         int input = 36_000_000;
 
-        visits(input, Day20::presentsInfinite);
-        visits(input, Day20::presentsFinite);
+        Timer.run(() -> visits(input, Day20::presentsInfinite));
+        Timer.run(() -> visits(input, Day20::presentsFinite));
     }
 
     private static void visits(int input, Function<Integer, Long> presents) {
-        int houseNumber = 0;
-        boolean found = false;
-        long presentsDelivered = 0L;
+        var houseNumber = 0;
+        var found = false;
+        var presentsDelivered = 0L;
         while (!found) {
             houseNumber++;
             System.out.print("\r" + houseNumber);
@@ -25,13 +26,13 @@ public class Day20 {
         System.out.println("\rHouse number " + houseNumber + " receives " + presentsDelivered);
     }
 
-    public static long presentsInfinite(int houseNumber) {
+    static long presentsInfinite(int houseNumber) {
         return Factors.list(houseNumber).stream()
                 .mapToLong(i -> i)
                 .sum() * 10;
     }
 
-    public static long presentsFinite(int houseNumber) {
+    static long presentsFinite(int houseNumber) {
         return Factors.list(houseNumber).stream()
                 .filter(i -> i * 50 >= houseNumber)
                 .mapToLong(i -> i)
