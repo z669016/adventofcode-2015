90c7d008001ba0ae5e0c17d4bc2f1ab00140fe60
Rene van Putten <z669016@gmail.com>
2023-08-14 16:45:16 +0200

    Added timer for Runnable

--- src/main/java/com/putoet/Timer.java
+++ src/main/java/com/putoet/Timer.java
@@ -10,4 +10,11 @@ public class Timer {
         System.out.println("Execution time: " + (end - start) / 1_000_000 + " ms");
         return value;
     }
+
+    public static void run(Runnable runnable ) {
+        final long start = System.nanoTime();
+        runnable.run();
+        final long end = System.nanoTime();
+        System.out.println("Execution time: " + (end - start) / 1_000_000 + " ms");
+    }
 }
